414b2a480cd6c542eee8cce8f99bc3c94bf15d74
marcosmedeirros <marcos.medeiros@acad.ufsm.com.br>
2026-02-02 09:59:53 -0300

    Ajustes

--- includes/head-pwa.php
+++ includes/head-pwa.php
@@ -6,13 +6,13 @@
 ?>
   <!-- PWA Meta Tags -->
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes">
-  <meta name="theme-color" content="#fc0025">
+  <meta name="theme-color" content="#000000">
   <meta name="apple-mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
   <meta name="apple-mobile-web-app-title" content="FBA Manager">
   <meta name="mobile-web-app-capable" content="yes">
   <meta name="format-detection" content="telephone=no">
-  <meta name="msapplication-TileColor" content="#fc0025">
+  <meta name="msapplication-TileColor" content="#000000">
   <meta name="msapplication-tap-highlight" content="no">
   
   <!-- PWA Manifest -->
@@ -25,11 +25,4 @@
   <!-- Apple Touch Icons -->
   <link rel="apple-touch-icon" sizes="180x180" href="/img/icons/icon-180.png?v=4">
   
-  <!-- Apple Splash Screens -->
-  <link rel="apple-touch-startup-image" href="/img/splash-640x1136.png" media="(device-width: 320px) and (device-height: 568px)">
-  <link rel="apple-touch-startup-image" href="/img/splash-750x1334.png" media="(device-width: 375px) and (device-height: 667px)">
-  <link rel="apple-touch-startup-image" href="/img/splash-1242x2208.png" media="(device-width: 414px) and (device-height: 736px)">
-  <link rel="apple-touch-startup-image" href="/img/splash-1125x2436.png" media="(device-width: 375px) and (device-height: 812px)">
-  <link rel="apple-touch-startup-image" href="/img/splash-1536x2048.png" media="(device-width: 768px) and (device-height: 1024px)">
-  <link rel="apple-touch-startup-image" href="/img/splash-1668x2224.png" media="(device-width: 834px) and (device-height: 1112px)">
-  <link rel="apple-touch-startup-image" href="/img/splash-2048x2732.png" media="(device-width: 1024px) and (device-height: 1366px)">
+  <!-- Apple Splash Screens removidos para usar plano de fundo do app -->
