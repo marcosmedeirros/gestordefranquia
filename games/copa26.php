ae61d5e9400e5f67353886ca87d16d08a2e19ec1
marcosmedeirros <marcos.medeiros@acad.ufsm.br>
2026-06-13 13:01:30 -0300

    ajuste

--- games/copa26.php
+++ games/copa26.php
@@ -1,5 +1,6 @@
 ﻿<?php
 session_start();
+date_default_timezone_set('America/Sao_Paulo');
 require 'core/conexao.php';
 if (!isset($_SESSION['user_id'])) { header("Location: auth/login.php"); exit; }
 
